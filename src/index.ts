c9c77d241ea43a1687aca729d9c24f728e12d8da
galenzo17 <bereciartua.agustin@gmail.com>
2024-03-28 20:06:07 -0300

    feat:refactor setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { Glob } from "bun";
 import ollama from "ollama";
-import axios from "axios";
 import { QdrantClient } from "@qdrant/js-client-rest";
+import { v4 as uuidv4 } from "uuid";
 
-const chunklengths = [5, 10, 25, 50, 100, 250, 500, 1000, 5000];
-const chunkoverlaps = [0, 3, 5, 10, 25, 50, 100, 500];
+const chunklengths = [50];
+const chunkoverlaps = [20];
+const client = new QdrantClient({
+  url: process.env.QDRANT_HOST || "http://127.0.0.1:6333",
+  // Asegúrate de incluir tu API key si tu instancia de Qdrant la requiere
+  apiKey: process.env.QDRANT_API_KEY,
+});
+const qdrantHost = process.env.QDRANT_HOST;
+const collectionName = "payout_collection";
+
+console.log(qdrantHost);
+
+const glob = new Glob("*.txt");
 
 function chunker(
   text: string,
@@ -12,7 +23,7 @@ function chunker(
   overlapWords: number
 ): string[] {
   const words = text.match(/[\w']+(?:[.,!?])?|\S/g) || [];
-
+  console.log("words", words.length);
   const chunks: string[] = [];
   for (let i = 0; i < words.length; i += wordsPerChunk - overlapWords) {
     let endIndex = i + wordsPerChunk;
@@ -25,93 +36,63 @@ function chunker(
   return chunks;
 }
 
-const qdrantHost = process.env.QDRANT_HOST;
-const collectionName = "my_collection";
-const qdrantApiKey = process.env.QDRANT_API_KEY;
-console.log(qdrantHost);
+const chunkLength = 50;
+const chunkOverlap = 20;
+async function processFile(file: string) {
+  console.log(`Text File: ${file}`);
+  const text = await Bun.file(`${file}`).text();
+  console.log("textlength", text.length);
+  const chunks = chunker(text, chunkLength, chunkOverlap);
+  const allChunks = [];
 
-const glob = new Glob("*.txt");
-
-for (const length of chunklengths) {
-  console.log(`Chunk Length: ${length}`);
-  for (const overlap of chunkoverlaps) {
-    console.log(`Chunk Overlap: ${overlap}`);
-    if (overlap < length) {
-      const allChunks = [];
-
-      for await (const file of glob.scan("src/scripts")) {
-        console.log(`Text File: ${file}`);
-        const scriptText = await Bun.file(`scripts/${file}`).toString();
-        const chunks = chunker(scriptText, length, overlap);
-        for await (const chunk of chunks) {
-          const embed = (await ollama.embeddings({
-            model: "nomic-embed-text",
-            prompt: chunk,
-          })).embedding;
-          // const embed = ""
-          const chunkjson = { text: chunk, embed: embed, file: file };
-          allChunks.push(chunkjson);
-          const data = {
-            points: [
-              {
-                id: `point_${Date.now()}`, // Ejemplo de generación de un ID simple basado en el tiempo
-                vector: embed,
-                payload: {
-                  text: chunk,
-                  file: file,
-                },
-              },
-            ],
-          };
-          try {
-            await saveToQdrant(data);
-          } catch (error) {
-            console.log(error);
-          }
-        }
-      }
-      await Bun.write(
-        `embedding-l${length}-o${overlap}.json`,
-        JSON.stringify(allChunks, null, 2)
-      );
-    }
+  for (const chunk of chunks) {
+    const embed = await generateEmbedding(chunk);
+    const chunkData = buildChunkData(file, chunk, embed);
+    allChunks.push(chunkData);
+    await saveToQdrant(chunkData);
   }
+
+  return allChunks;
 }
 
-type QdrantPointPayload = {
-  text: string;
-  file: string;
-  // Puedes añadir más campos según sea necesario,
-  // basado en tu esquema de colección en Qdrant
-};
-
-type QdrantPoint = {
-  id: string; // o number, dependiendo de cómo gestionas los IDs
-  vector: number[];
-  payload?: QdrantPointPayload;
-};
-
-type QdrantInsertData = {
-  points: QdrantPoint[];
-};
-
-const client = new QdrantClient({
-  url: process.env.QDRANT_HOST || "http://127.0.0.1:6333",
-  // Asegúrate de incluir tu API key si tu instancia de Qdrant la requiere
-  apiKey: process.env.QDRANT_API_KEY,
-});
-
-async function saveToQdrant(data: QdrantInsertData) {
-  console.log(process.env.QDRANT_HOST);
-  try {
-    // Asumiendo que `data.points` contiene tus puntos a insertar
-    for (const point of data.points) {
-      await client.upsert(collectionName, {
-        points: [point],
-      });
-    }
-    console.log(`Saved to Qdrant successfully.`);
-  } catch (error) {
-    console.error(`Error saving to Qdrant: ${error}`);
-  }
+async function generateEmbedding(chunk: string) {
+  const embeddingResult = await ollama.embeddings({
+    model: "llama2",
+    prompt: chunk,
+  });
+  return embeddingResult.embedding;
 }
+
+function buildChunkData(file: string, chunk: string, embed: number[]) {
+  return {
+    id: uuidv4(),
+    vector: embed,
+    payload: { text: chunk, file: file },
+  };
+}
+
+async function saveToQdrant(data) {
+  console.log("Saving to Qdrant:", data.payload.text);
+  const payload = {
+    points: [{ id: data.id, payload: data.payload, vector: data.vector }],
+  };
+  await client.upsert(collectionName, payload);
+  console.log(`Saved to Qdrant successfully.`);
+}
+
+async function main() {
+  console.time("Execution Time");
+  const allChunks = [];
+  for await (const file of new Glob("src/scripts/*.txt").scan()) {
+    const fileChunks = await processFile(file);
+    allChunks.push(...fileChunks);
+  }
+
+  await Bun.write(
+    `embedding-l${chunkLength}-o${chunkOverlap}.json`,
+    JSON.stringify(allChunks, null, 2)
+  );
+  console.timeEnd("Execution Time");
+}
+
+await main();
